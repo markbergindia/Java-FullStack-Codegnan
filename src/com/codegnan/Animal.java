c03345e64fba20367edaf1713f113a23ae04a1d0
sriram <ramanaboinasriram@gmail.com>
2026-02-21 23:31:27 +0530

    "new added file"

---
+++ src/com/codegnan/Animal.java
@@ -0,0 +1,18 @@
+package com.codegnan;
+public class Animal {
+	public static void main(String [] args) {
+		int arr [] = {56,89,14,69,66};
+		int target = 66;
+		int index=-1;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==target) {index=1;
+			break;
+
+			}
+		}
+		System.out.println(index!=1? "found":"notfound");
+		
+			
+	}
+	
+}
