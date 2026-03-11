c9a6bbb163c1f9874760c6913aa0d5f6e74fbc02
sriram <ramanaboinasriram@gmail.com>
2026-03-11 22:08:37 +0530

    "new added files"

--- src/com/codegnan/dsa/FindFirstandLastIndex.java
+++ src/com/codegnan/dsa/FindFirstandLastIndex.java
@@ -1,19 +1,48 @@
 package com.codegnan.dsa;
 
 public class FindFirstandLastIndex {
-	public static void main(String[] args) {
-	int nums [] = {5,7,7,8,8,10};
-	int target = 8 ;
-	int start = 0 ;
-	int end = nums.length-1;
-	while(start <= end) {
-		int mid = start+end/2;
-		if(nums[mid] == target) {
-			
-		}
-	}
-	
-		
-		
-	}
+
+    public static int[] searchRange(int nums[], int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+
+    public static int findBound(int nums[], int target, boolean isFirst) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                index = mid;
+
+                if (isFirst) {
+                    right = mid - 1;   
+                } else {
+                    left = mid + 1;    
+                }
+
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {5, 7, 7, 8, 8, 11};
+        int target = 8;
+
+        int result[] = searchRange(nums, target);
+
+        System.out.println("First: " + result[0]);
+        System.out.println("Last: " + result[1]);
+    }
 }
---
+++ src/com/codegnan/dsa/FindMinimumInRotatedArray.java
@@ -0,0 +1,30 @@
+package com.codegnan.dsa;
+
+public class FindMinimumInRotatedArray {
+
+    public static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] > nums[end]) {
+                
+                start = mid + 1;
+            } else {
+                
+                end = mid;
+            }
+        }
+
+        return nums[start];
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {4,5,6,7,0,1,2};
+
+        int min = findMin(nums);
+        System.out.println("Minimum element: " + min);
+    }
+}
